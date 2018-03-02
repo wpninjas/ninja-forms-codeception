dd18f6206b58c21fed28df15874b8ef80076de33
KR Moorhouse <spkr4thded@gmail.com>
2018-03-02 16:03:10 -0500

    Added function logOut to bootstrap file for calling from tests.

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,2 +1,12 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+
+/**
+ * Function to log the user out of WordPress.
+ * @param $I instanceof AcceptanceTester.
+ */
+function logOut( $I ) {
+    $I->executeJS( 'jQuery("#wp-admin-bar-user-actions").parent().removeClass("ab-sub-wrapper");' );
+    $I->click( '#wp-admin-bar-logout > a' );
+    $I->wait( 2 );
+}
